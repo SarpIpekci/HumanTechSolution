969032fc8edf3208a6dc043b7bed06c4ab255087
Sarp <Sarp@DESKTOP-V628AAG>
2022-08-31 12:32:49 +0300

    Add project files.

---
+++ HumanTech/Controllers/StockController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HumanTech.Models;
+
+namespace HumanTech.Controllers
+{
+    public class StockController : Controller
+    {
+        private readonly StockDbContext _context;
+
+        public StockController(StockDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stock
+        public async Task<IActionResult> Index()
+        {
+            return _context.Stocks != null ?
+                        View(await _context.Stocks.ToListAsync()) :
+                        Problem("Entity set 'StockDbContext.Stocks'  is null.");
+        }
+
+        // GET: Stock/AddOrEdit
+        //Update and Insert combine.
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Stock());
+            else
+                return View(_context.Stocks.Find(id));
+        }
+
+        // POST: Stock/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("StockID,StockName,Quantity,UnitPrice,IsActive,CreatedDate")] Stock stock)
+        {
+            //Validation control
+            if (ModelState.IsValid)
+            {
+                //Check id is exist
+                //If exist intert
+                if (stock.StockID == 0)
+                {
+                    stock.IsActive = true;
+                    stock.CreatedDate = DateTime.Now;
+                    _context.Add(stock);
+                }
+                //If not exist update
+                else
+                    _context.Update(stock);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(stock);
+        }
+
+        // POST: Stock/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Stocks == null)
+            {
+                return Problem("Entity set 'StockDbContext.Stocks'  is null.");
+            }
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock != null)
+            {
+                _context.Stocks.Remove(stock);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ HumanTech/Models/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace HumanTech.Models
+{
+    public class Stock
+    {
+        [Key]
+        public int StockID { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Stock Name")]
+        [Required]
+        public string StockName { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [DisplayName("Unit Price")]
+        [Required]
+        public int UnitPrice { get; set; }
+        public bool? IsActive { get; set; }
+        [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}")]
+        public DateTime? CreatedDate { get; set; }
+    }
+}
